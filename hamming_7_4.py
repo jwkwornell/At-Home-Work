a65ae8d230cb62df27d7deb83670dac23fb9d054
jwkwornell <109394281+jwkwornell@users.noreply.github.com>
2022-07-28 23:18:02 -0400

    Hamming(7,4) working now.

--- hamming_7_4.py
+++ hamming_7_4.py
@@ -1,113 +1,28 @@
-def calcRedundantBits(m):
- 
-    # Use the formula 2 ^ r >= m + r + 1
-    # to calculate the no of redundant bits.
-    # Iterate over 0 .. m and return the value
-    # that satisfies the equation
- 
-    for i in range(m):
-        if(2**i >= m + i + 1):
-            return i
- 
-def posRedundantBits(data, r):
- 
-    # Redundancy bits are placed at the positions
-    # which correspond to the power of 2.
-    j = 0
-    k = 1
-    m = len(data)
-    res = ''
- 
-    # If position is power of 2 then insert '0'
-    # Else append the data
-    for i in range(1, m + r+1):
-        if(i == 2**j):
-            res = res + '0'
-            j += 1
-        else:
-            res = res + data[-1 * k]
-            k += 1
- 
-    # The result is reversed since positions are
-    # counted backwards. (m + r+1 ... 1)
-    return res[::-1]
- 
- 
-def calcParityBits(arr, r):
-    n = len(arr)
- 
-    # For finding rth parity bit, iterate over
-    # 0 to r - 1
-    for i in range(r):
-        val = 0
-        for j in range(1, n + 1):
- 
-            # If position has 1 in ith significant
-            # position then Bitwise OR the array value
-            # to find parity bit value.
-            if(j & (2**i) == (2**i)):
-                val = val ^ int(arr[-1 * j])
-                # -1 * j is given since array is reversed
- 
-        # String Concatenation
-        # (0 to n - 2^r) + parity bit + (n - 2^r + 1 to n)
-        arr = arr[:n-(2**i)] + str(val) + arr[n-(2**i)+1:]
-    return arr
- 
- 
-def detectError(arr, nr):
-    n = len(arr)
-    res = 0
- 
-    # Calculate parity bits again
-    for i in range(nr):
-        val = 0
-        for j in range(1, n + 1):
-            if(j & (2**i) == (2**i)):
-                val = val ^ int(arr[-1 * j])
- 
-        # Create a binary no by appending
-        # parity bits together.
- 
-        res = res + val*(10**i)
- 
-    # Convert binary to decimal
-    return int(str(res), 2)
- 
- 
-# # Enter the data to be transmitted
-# data = '1011001'
- 
-# # Calculate the no of Redundant Bits Required
-# m = len(data)
-# r = calcRedundantBits(m)
- 
-# # Determine the positions of Redundant Bits
-# arr = posRedundantBits(data, r)
- 
-# # Determine the parity bits
-# arr = calcParityBits(arr, r)
- 
-# # Data to be transferred
-# print("Data transferred is " + arr) 
- 
-# # Stimulate error in transmission by changing
-# # a bit value.
-# # 10101001110 -> 11101001110, error in 10th position.
- 
-# arr = '11101001110'
-# print("Error Data is " + arr)
-# correction = detectError(arr, r)
-# if(correction==0):
-#     print("There is no error in the received message.")
-# else:
-#     print("The position of error is ",len(arr)-correction+1,"from the left")
+K = 4
 
-data = '0100'
-print(data)
-m = len(data)
-r = calcRedundantBits(m)
+def parity(bin_str, indicies):
+    # Compute the parity bit for the given string and indicies
+    sub = ''
+    for i in indicies:
+        sub += bin_str[i]
+    return str(str.count(sub, '1') % 2)
 
-arr = posRedundantBits(data, r)
-arr = calcParityBits(arr, r)
-print(arr)
+def hamming(bits):
+    # Return given 4 bits plus parity bits for bits (1,2,3), (2,3,4), and (1,3,4)
+    t1 = parity(bits, [0,1,3])
+    t2 = parity(bits, [0,2,3])
+    t3 = parity(bits, [1,2,3])
+    return t1 + t2 + bits[0] + t3 + bits[1:]
+
+def encode(s):
+    while len(s) >= K: 
+        nibble = s[0:K]
+        input(hamming(nibble))
+        s = s[K:]
+
+
+if __name__ == "__main__":
+    print("Enter Input String of bits - ") #just for testing
+    input_string = input().strip()
+    print(" Output - ",end=' ') #just for testing
+    encode(input_string)
